af71fa667af9730c4ca44ec5c3890821f03f7835
Tangguh80 <135090426+Tangguh80@users.noreply.github.com>
2023-12-21 11:58:35 +0700

    Add files via upload

---
+++ HalamanUtama/index.js
@@ -0,0 +1,461 @@
+/*====================================== animasi buntut ====================================*/
+
+let trails = [];
+
+for (let i = 0; i < 10; i++) {
+  let trail = document.createElement("div");
+  trail.className = "trail";
+  document.body.appendChild(trail);
+  trails.push(trail);
+}
+
+let currentTrailIndex = 0;
+
+document.addEventListener("mousemove", (event) => {
+  let trail = trails[currentTrailIndex];
+  trail.style.left = `${event.pageX}px`;
+  trail.style.top = `${event.pageY}px`;
+  trail.style.opacity = 1;
+  
+  // Move to the next trail div
+  currentTrailIndex++;
+  if (currentTrailIndex >= trails.length) {
+    currentTrailIndex = 0;
+  }
+
+  // Reset the trail div after 1 second
+  setTimeout(() => {
+    trail.style.opacity = 0;
+  }, 100);
+});
+
+
+
+
+
+
+/*====================================== animasi pada halaman pertama ====================================*/
+
+// Fungsi untuk menangani pengguliran
+function handleScroll() {
+  const sections = document.querySelectorAll("section"); // Ganti dengan elemen yang sesuai
+  const navigationLinks = document.querySelectorAll(".navigation ul li a");
+
+  sections.forEach((section, index) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (window.scrollY >= sectionTop - 100 && window.scrollY < sectionTop + sectionHeight - 100) {
+      navigationLinks[index].classList.add("active");
+    } else {
+      navigationLinks[index].classList.remove("active");
+    }
+  });
+}
+
+// Tambahkan event listener untuk peristiwa pengguliran
+window.addEventListener("scroll", handleScroll);
+
+
+
+
+
+
+
+
+
+
+// Wait for the page to load
+window.addEventListener('load', function () {
+  // Add the "show" class to elements after a delay (you can adjust the delay)
+  setTimeout(function () {
+    document.querySelector('.social-icons').classList.add('show');
+    document.querySelector('.email-container').classList.add('show');
+  }, 500); // Adjust the delay as needed
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    var navigation = document.querySelector(".navigation ul");
+    navigation.style.opacity = "1"; // Atur opacity menjadi 1 untuk munculkan elemen
+    navigation.style.transform = "translateY(0)"; // Kembalikan posisi ke normal
+  }, 1500); // Tunggu 2 detik sebelum memulai animasi
+});
+
+
+
+
+
+
+
+
+
+
+/*====================================== memunculkan side bar pada tampilan mobile ====================================*/
+
+// Pilih elemen-elemen yang dibutuhkan
+const menuToggle = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+const navigation = document.querySelector(".navigation");
+
+// Fungsi untuk toggle sidebar
+function toggleSidebar() {
+  if (sidebar.classList.contains('sidebar-visible')) {
+    sidebar.classList.remove('sidebar-visible');
+    menuToggle.innerHTML = '<ion-icon name="menu-outline"></ion-icon>';
+  } else {
+    sidebar.classList.add('sidebar-visible');
+    menuToggle.innerHTML = '<ion-icon name="close-circle-outline"></ion-icon>';
+  }
+}
+
+// Atur event listener untuk menu toggle
+menuToggle.addEventListener("click", toggleSidebar);
+
+// Fungsi untuk mengecek dan menyesuaikan tampilan berdasarkan lebar layar
+function checkWidth() {
+  if (window.innerWidth <= 892) {
+    if (!sidebar.classList.contains('sidebar-visible')) {
+      navigation.style.display = "none";
+    }
+  } else {
+    navigation.style.display = "block";
+    sidebar.classList.remove('sidebar-visible'); // Sembunyikan sidebar
+    menuToggle.innerHTML = '<ion-icon name="menu-outline"></ion-icon>';
+  }
+}
+
+// Panggil fungsi checkWidth saat halaman dimuat
+checkWidth();
+
+// Pantau perubahan lebar layar dan sesuaikan tampilan
+window.addEventListener("resize", checkWidth);
+
+
+
+
+
+
+
+
+
+/*====================================== script untuk mencari kata ====================================*/
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const searchBox = document.querySelector('.search-box');
+  
+  searchBox.addEventListener('input', function() {
+      removeHighlights();
+
+      const searchText = this.value.trim();
+      if (searchText) {
+          highlightText(document.body, searchText);
+      }
+  });
+
+  function removeHighlights() {
+      document.querySelectorAll('.highlight').forEach(el => {
+          const parent = el.parentNode;
+          parent.replaceChild(document.createTextNode(el.textContent), el);
+          parent.normalize();
+      });
+  }
+
+  function highlightText(node, searchText) {
+      const regex = new RegExp(searchText, 'gi');
+
+      Array.from(node.childNodes).forEach(child => {
+          if (child.nodeType === 3) { // Node.TEXT_NODE
+              const matches = child.nodeValue.match(regex);
+              if (matches) {
+                  const frag = document.createDocumentFragment();
+                  let lastIdx = 0;
+                  child.nodeValue.replace(regex, (match, offset) => {
+                      const part = document.createTextNode(child.nodeValue.slice(lastIdx, offset));
+                      const highlighted = document.createElement('span');
+                      highlighted.className = 'highlight';
+                      highlighted.textContent = match;
+
+                      frag.appendChild(part);
+                      frag.appendChild(highlighted);
+                      lastIdx = offset + match.length;
+                  });
+                  frag.appendChild(document.createTextNode(child.nodeValue.slice(lastIdx)));
+                  node.replaceChild(frag, child);
+
+                  const firstHighlight = document.querySelector('.highlight');
+                  if (firstHighlight) {
+                      firstHighlight.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                  }
+              }
+          } else if (child.nodeType === 1) { // Node.ELEMENT_NODE
+              highlightText(child, searchText);
+          }
+      });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*====================================== memeunculkan search box =========================================*/
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const searchBox = document.querySelector('.search-box');
+    const closeSearch = document.querySelector('.close-search');
+    const cariButton = document.querySelector('.cari');
+    const searchContainer = document.querySelector('.search-container');
+    const navigationLink = document.querySelector('.navigation a'); // Assuming this is the correct selector
+
+    // Toggle search box on clicking 'cari' button
+    cariButton.addEventListener('click', function() {
+        if (searchContainer.classList.contains('active')) {
+            hideSearchBox();
+        } else {
+            showSearchBox();
+        }
+    });
+
+    // Function to show the search box
+    function showSearchBox() {
+        searchContainer.classList.add('active');
+        searchBox.style.transform = 'translateX(0%)';
+        closeSearch.style.transform = 'translateX(0%)';
+        navigationLink.style.zIndex = '1'; // Move .navigation a to the back
+    }
+
+    // Function to hide the search box
+    function hideSearchBox() {
+        searchBox.style.transform = 'translateX(-120%)';
+        closeSearch.style.transform = 'translateX(-900%)';
+        setTimeout(function() {
+            searchContainer.classList.remove('active');
+        }, 300); // Assumed duration of the animation
+    }
+
+    closeSearch.addEventListener('click', hideSearchBox);
+
+    // Hide search box and adjust navigation link based on viewport size
+    function adjustLayoutBasedOnViewport() {
+        const viewportWidth = window.innerWidth;
+        if (viewportWidth <= 1121) {
+            hideSearchBox();
+            navigationLink.style.zIndex = '1'; // Move .navigation a to the front
+        }
+    }
+
+    window.addEventListener('resize', adjustLayoutBasedOnViewport);
+
+    // Initial adjustment on page load
+    adjustLayoutBasedOnViewport();
+});
+
+
+
+
+/*====================================== menyembunyikan search box jika navigation overload ====================================*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cariButton = document.querySelector('.cari');
+  var closeSearch = document.querySelector('.close-search');
+  var searchContainer = document.querySelector('.search-container');
+  var navigation = document.querySelector('.navigation');
+
+  // Function to show the search box
+  function showSearchBox() {
+    searchContainer.style.zIndex = "2";
+    navigation.style.zIndex = "1";
+  }
+
+  // Function to hide the search box
+  function hideSearchBox() {
+    searchContainer.style.zIndex = "1";
+    navigation.style.zIndex = "2";
+  }
+
+  // Add click event listener to .cari button
+  cariButton.addEventListener('click', showSearchBox);
+
+  // Add click event listener to .close-search button
+  closeSearch.addEventListener('click', hideSearchBox);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*====================================== TOMBOL TERANG DAN GELAP ====================================*/
+// Mendapatkan elemen tombol terang
+let terang = document.querySelector(".terang");
+// Mendapatkan elemen gambar logo
+let logo = document.getElementById("logo");
+// Mendapatkan elemen tombol kontras pada sidebar dan teksnya
+let kontras = document.querySelector(".sidebar ul li a[href='#contrast']");
+let modeText = kontras.querySelector("span");
+
+// Mendefinisikan variabel untuk menyimpan status warna, rotasi, dan status logo
+let warna = "normal"; // normal atau terbalik
+let rotasi = 0; // derajat rotasi
+let logoStatus = "logo1"; // logo1 atau logo2
+
+// Fungsi untuk mengganti tema dan teks
+function toggleTema() {
+  let root = document.documentElement;
+
+  if (warna == "normal") {
+    root.style.setProperty("--white", "#000000");
+    root.style.setProperty("--black1", "#ffffff");
+    root.style.setProperty("--black2", "#f5f5f5");
+    warna = "terbalik";
+    modeText.textContent = "Mode Malam"; // Mengubah teks menjadi Mode Malam
+  } else {
+    root.style.setProperty("--white", "#ffffff");
+    root.style.setProperty("--black1", "#000000");
+    root.style.setProperty("--black2", "#0f0f0f");
+    warna = "normal";
+    modeText.textContent = "Mode Siang"; // Mengubah teks menjadi Mode Siang
+  }
+}
+
+// Menambahkan event listener untuk tombol terang
+terang.addEventListener("click", function() {
+  toggleTema();
+
+  // Mengubah rotasi tombol terang dengan CSS transform
+  rotasi += 180; // Menambahkan 180 derajat ke rotasi
+  terang.style.transform = `rotate(${rotasi}deg)`; // Mengatur properti transform
+
+  // Mengganti logo
+  if (logoStatus == "logo1") {
+    logo.src = "TS LOGO2.png";
+    logoStatus = "logo2";
+  } else {
+    logo.src = "TS LOGO.png";
+    logoStatus = "logo1";
+  }
+});
+
+// Menambahkan event listener untuk tombol kontras
+kontras.addEventListener("click", function(event) {
+  event.preventDefault(); 
+  toggleTema();
+});
+
+
+document.querySelector('.nav-container .navigation .logo a.cari').addEventListener('click', function() {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*====================================== NOTIF AKTIF SIDEBAR ====================================*/
+function moveNotification(topValue) {
+  const notification = document.getElementById("notification");
+
+  // Hapus kelas animasi sebelum mengatur ulang animasi
+  notification.classList.remove("zoomIn", "zoomOut");
+
+  // Tambahkan kelas animasi zoomOut untuk menghilangkan notifikasi saat tombol ditekan
+  notification.classList.add("zoomOut");
+
+  // Setelah animasi zoomOut selesai, pindahkan notifikasi ke posisi yang sesuai
+  setTimeout(() => {
+    notification.style.top = topValue + "px";
+
+    // Hapus kelas animasi zoomOut dan tambahkan kelas animasi zoomIn
+    notification.classList.remove("zoomOut");
+    notification.classList.add("zoomIn");
+  }, 100); // Ubah sesuai dengan durasi animasi Anda (dalam milidetik)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LoadingConfirm/loading.js
@@ -0,0 +1,365 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const container2 = document.getElementById("container2");
+    const buttonAgree = document.querySelector(".button-agree");
+    const overlay = document.querySelector('.overlay');
+
+    // Check if container2 is present
+    if (container2) {
+        // Wait for the container2 and overlay animation to start and then pause all animations
+        setTimeout(pauseAllAnimations, 3500); // Adjust the time as per your requirement
+    }
+
+    // Event listener for the agree button
+    buttonAgree.addEventListener("click", function() {
+        // Resume or reverse animations for container2 and overlay
+        container2.classList.add('slideDownReverse');
+        overlay.classList.add('fadeOutOverlay');
+
+        // Resume or start other animations
+        resumeOrStartOtherAnimations();
+    });
+
+    function pauseAllAnimations() {
+        // Select all elements that need to pause animating
+        const animatedElements = document.querySelectorAll('.square, .square2, .Font-image, .Bintang-image, .line1, .line2');
+
+        // Pause the animation of each element
+        animatedElements.forEach(function(element) {
+            element.style.animationPlayState = 'paused';
+        });
+    }
+
+    function resumeOrStartOtherAnimations() {
+        // Select all elements that had their animations paused
+        const animatedElements = document.querySelectorAll('.square, .square2, .Font-image, .Bintang-image, .line1, .line2');
+
+        // Resume or start the animation for each element
+        animatedElements.forEach(function(element) {
+            element.style.animationPlayState = 'running'; // Resume paused animations
+
+            // If you have specific animations to start, add them here
+            // For example:
+            // if (element.classList.contains('some-class')) {
+            //     element.classList.add('start-some-animation');
+            // }
+        });
+
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.querySelector('.button-disagree');
+    var overlay = document.getElementById('overlay2');
+    var moreInfoContainer = document.getElementById('moreInfoContainer');
+
+    button.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        overlay.style.animation = 'fadeInOverlay2 0.5s ease-out 0s forwards';
+        moreInfoContainer.style.display = 'block';
+        moreInfoContainer.classList.remove('fadeOut');
+    });
+
+    // Close functionality on clicking the overlay
+    overlay.addEventListener('click', function() {
+        this.style.animation = 'fadeOutOverlay2 0.5s ease-out forwards';
+        moreInfoContainer.classList.add('fadeOut');
+        setTimeout(() => {
+            this.style.display = 'none';
+            moreInfoContainer.style.display = 'none';
+        }, 500); // matches the animation duration
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var rejectButton = document.querySelector('.button-Reject');
+
+    rejectButton.addEventListener('click', function() {
+        // Reload the current page without using the cache
+        window.location.reload(true);
+    });
+});
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var acceptButton = document.querySelector('.button-agree2');
+
+    acceptButton.addEventListener('click', function() {
+        // Query all switch inputs and set them to checked
+        document.querySelectorAll('.big-switch input, .small-switch input').forEach(function(switchInput) {
+            switchInput.checked = true;
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const container2 = document.getElementById("container2");
+    const overlay1 = document.querySelector('.overlay');
+    const overlay2 = document.querySelector('.overlay2');
+    const moreInfoContainer = document.querySelector(".moreInfo-container");
+    const buttonAgree2 = document.querySelector('.button-agree2');
+    const buttonAccSelect = document.querySelector('.button-AccSelect');
+
+    // Function to toggle all switches to 'checked'
+    function toggleAllSwitches() {
+        document.querySelectorAll('.big-switch input, .small-switch input').forEach(function(switchInput) {
+            switchInput.checked = true;
+        });
+    }
+
+    // Function to check if any switch is checked
+    function isAnySwitchChecked() {
+        return Array.from(document.querySelectorAll('.big-switch input, .small-switch input')).some(input => input.checked);
+    }
+
+    // Function to close the moreInfoContainer
+    function closeMoreInfoContainer() {
+        moreInfoContainer.classList.add('fadeOut');
+        setTimeout(() => {
+            moreInfoContainer.style.display = 'none';
+            closeOverlay2();
+        }, 500); // Duration of fadeOut animation for moreInfoContainer
+    }
+
+    // Function to close overlay2
+    function closeOverlay2() {
+        overlay2.style.animation = 'fadeOutOverlay2 0.7s ease-out forwards';
+        setTimeout(() => {
+            overlay2.style.display = 'none';
+            slideDownReverseContainer2();
+        }, 700); // Duration of fadeOut animation for overlay2
+    }
+
+    // Function to reverse the animation of container2 and proceed to close overlay1
+    function slideDownReverseContainer2() {
+        container2.classList.add('slideDownReverse');
+        setTimeout(() => {
+            container2.style.display = 'none';
+            closeFirstOverlay();
+        }, 600); // Duration of slideDownReverse animation for container2
+    }
+
+    // Function to close the first overlay (overlay1)
+    function closeFirstOverlay() {
+        overlay1.classList.add('fadeOutOverlay');
+        setTimeout(resumeOrStartOtherAnimations, 600); // Duration of fadeOutOverlay animation time
+    }
+
+    // Function to resume or start other animations
+    function resumeOrStartOtherAnimations() {
+        const animatedElements = document.querySelectorAll('.square, .square2, .Font-image, .Bintang-image, .line1, .line2');
+        animatedElements.forEach(function(element) {
+            element.style.animationPlayState = 'running';
+        });
+    }
+
+    // Event listener for the "Accept all" button
+    buttonAgree2.addEventListener("click", function() {
+        toggleAllSwitches();
+        setTimeout(closeMoreInfoContainer, 500); // Adjust delay based on switch toggle animation
+    });
+
+    // Event listener for the "Accept selected" button
+    buttonAccSelect.addEventListener("click", function() {
+        if (isAnySwitchChecked()) {
+            setTimeout(closeMoreInfoContainer, 500); // Adjust delay as needed
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Selecting big switches (1, 2, 3) and small switches (4, 5, 6)
+    const bigSwitch1 = document.querySelector('.container-switch1 .big-switch input');
+    const bigSwitch2 = document.querySelector('.container-switch2 .big-switch input');
+    const bigSwitch3 = document.querySelector('.container-switch3 .big-switch input');
+    const smallSwitch4 = document.querySelector('.container-switch4 .small-switch input');
+    const smallSwitch5 = document.querySelector('.container-switch5 .small-switch input');
+    const smallSwitch6 = document.querySelector('.container-switch6 .small-switch input');
+
+    // Function to synchronize two switches bidirectionally
+    function syncSwitches(switch1, switch2) {
+        switch1.addEventListener('change', function() {
+            switch2.checked = this.checked;
+        });
+
+        switch2.addEventListener('change', function() {
+            switch1.checked = this.checked;
+        });
+    }
+
+    // Synchronizing the states of corresponding switches
+    syncSwitches(bigSwitch1, smallSwitch4);
+    syncSwitches(bigSwitch2, smallSwitch5);
+    syncSwitches(bigSwitch3, smallSwitch6);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    var button = document.querySelector('.button-AccSelect');
+
+    function updateButtonClass() {
+        var isAnyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+        button.classList.toggle('hover-green', isAnyChecked);
+        button.classList.toggle('hover-red', !isAnyChecked);
+    }
+
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', updateButtonClass);
+    });
+
+    updateButtonClass(); // Run on load
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // ... kode lainnya ...
+
+    const animatedElements = document.querySelectorAll('.square, .square2, .Font-image, .Bintang-image, .line1, .line2');
+    let animationsDone = {}; // Objek untuk menyimpan status animasi
+
+    // Inisialisasi status animasi untuk setiap elemen
+    animatedElements.forEach(function(element) {
+        animationsDone[element.className] = false; // Atur semula status animasi sebagai false
+        element.addEventListener('animationend', function() {
+            animationsDone[element.className] = true; // Perbarui status animasi menjadi true ketika selesai
+            checkAnimationsAndRedirect();
+        });
+    });
+
+    function checkAnimationsAndRedirect() {
+        // Cek jika semua animasi telah selesai
+        const allAnimationsDone = Object.values(animationsDone).every(status => status);
+        if (allAnimationsDone) {
+            // Menetapkan delay sebelum pengalihan halaman
+            setTimeout(function() {
+                window.location.href = "file:///C:/web-saya3/Portofolio/HalamanUtama/index.html";
+            }, 3000); // Delay 3000 milidetik (3 detik)
+        }
+    }
+
+    // ... kode lainnya ...
+});
