f21cf5639a9003164829897a3e34bbbcdd52450c
Tangguh80 <135090426+Tangguh80@users.noreply.github.com>
2023-12-21 19:43:58 +0700

    Update Loading.js

--- LoadingConfirm/Loading.js
+++ LoadingConfirm/Loading.js
@@ -356,7 +356,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (allAnimationsDone) {
             // Menetapkan delay sebelum pengalihan halaman
             setTimeout(function() {
-            window.location.href = "/HalamanUtama/Index.html";
+            window.location.href = "HalamanUtama/Index.html";
             }, 3000); // Delay 3000 milidetik (3 detik)
         }
     }
